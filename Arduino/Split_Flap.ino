41241a931a749f66b2335425cb66100eef19f98b
g000ze <stefan@frech.ch>
2024-12-31 10:57:04 +0100

    better update_ms_pos support and other improvements

--- Arduino/Split_Flap.ino
+++ Arduino/Split_Flap.ino
@@ -1,4 +1,4 @@
-#include <inputs.ino>
+#include </Users/stefan/Documents/Arduino/Split_Flap/inputs/inputs.ino>
 #include <Wire.h>
 
 // motor and flap settings
@@ -22,31 +22,35 @@ const word ms_per_flap = ms_per_revolution / flaps; // microsteps performed by f
 const long speed = 28800 / microsteps;
 
 // Number of modules
-// Can be extended to 8 per Arduino.
-const byte modules = 6;
+const byte modules = 8;
 
 // array settings
-const byte hall_pin_0 = 4;
-const byte hall_pin_1 = 5;
-const byte hall_pin_2 = 6;
-const byte hall_pin_3 = 7;
-const byte hall_pin_4 = 8;
-const byte hall_pin_5 = 9;
-const byte hall_pin_6 = 10;
-const byte hall_pin_7 = 11;
+// As of Backplane V2 PCB
 
-const byte step_pin_0 = 18;
-const byte step_pin_1 = 19;
-const byte step_pin_2 = 20;
-const byte step_pin_3 = 21;
-const byte step_pin_4 = 22;
-const byte step_pin_5 = 23;
-const byte step_pin_6 = 24;
-const byte step_pin_7 = 25;
+//////////////////////////////
+const byte hall_pin_0 = 18;
+const byte hall_pin_1 = 19;
+const byte hall_pin_2 = 20;
+const byte hall_pin_3 = 21;
+const byte hall_pin_4 = 9;
+const byte hall_pin_5 = 10;
+const byte hall_pin_6 = 11;
+const byte hall_pin_7 = 12;
 
-// some other things
-unsigned long curr_time = 0;
-unsigned long prev_time = 0;
+const byte step_pin_0 = 15;
+const byte step_pin_1 = 14;
+const byte step_pin_2 = 23;
+const byte step_pin_3 = 22;
+const byte step_pin_4 = 5;
+const byte step_pin_5 = 4;
+const byte step_pin_6 = 17;
+const byte step_pin_7 = 16;
+//////////////////////////////
+
+/*
+const byte hall_pin_0 = 6;
+const byte step_pin_0 = 0;
+*/
 
 const byte hall_pin[modules] =
 {
@@ -56,8 +60,8 @@ const byte hall_pin[modules] =
     hall_pin_3,
     hall_pin_4,
     hall_pin_5,
-//    hall_pin_6,
-//    hall_pin_7,
+    hall_pin_6,
+    hall_pin_7,
 };
 
 const byte step_pin[modules] =
@@ -68,57 +72,48 @@ const byte step_pin[modules] =
     step_pin_3,
     step_pin_4,
     step_pin_5,
-//    step_pin_6,
-//    step_pin_7,
+    step_pin_6,
+    step_pin_7,
 };
+
+// some other things
+unsigned long curr_time = 0;
+unsigned long prev_time = 0;
 
 word cur_ms_pos[modules]          = {};
 word new_ms_pos[modules]          = {};
 int  ms_to_go[modules]            = {};
-
-byte cur_flap_pos[modules]        = {};
-byte new_flap_pos[modules]        = {};
-byte flap_pos_to_go[modules]      = {};
-
 char new_char[modules]            = {};
-
-int home_ms_pos[modules]          = {};
+bool set_ms_pos[modules]          = {};
 
 // Feineinstellung
-const word init_ms_pos[modules]   = {24, 24, 24, 24, 24, 32,};
-
-bool enabled[modules]             = {};
-
+const word init_ms_pos[modules]   = {50, 10, 10, 10, 10, 10, 10, 10};
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(9600);
 
+  /*
   // Join I2C bus as slave with uniqe address
   Wire.begin(0x0b);
   // set I2C receive and request functions
   Wire.onReceive(receive_i2c_event);
   Wire.onRequest(request_i2c_event);
-
+  */
   // setup pins and variables
   for(byte module = 0; module < modules; module++){
     pinMode(step_pin[module], OUTPUT);
     pinMode(hall_pin[module], INPUT);
 
+    // ascii 32 is space character
     new_char[module] = 32;
-
     cur_ms_pos[module] = 0;
+    set_ms_pos[module] = false;
     set_ms_to_go(module);
-
-    cur_flap_pos[module]    = flaps;
-    new_flap_pos[module]    = 0;
-    flap_pos_to_go[module]  = 0;
-
-    // get away from home for initial revolution on startup
-    home_ms_pos[module] = ms_per_flap;
-
-    enabled[module]  = true;
   }
 }
+
+// ToDo XXX
+//#include </Users/stefan/Documents/Arduino/Split_Flap/output/output.ino>
 
 void loop() {
   motor_run();
@@ -130,36 +125,12 @@ void motor_run(){
   if((curr_time - prev_time) >= speed){
     prev_time = curr_time;
     for(byte module = 0; module < modules; module++){
-      if(enabled[module]){
-        if(ms_to_go[module] > 0){
-          digitalWrite(step_pin[module], HIGH);
-          digitalWrite(step_pin[module], LOW);  
-          update_ms_pos(module);
-          update_flap_pos(module);
-        }
+      if(ms_to_go[module] > 0){
+        digitalWrite(step_pin[module], HIGH);
+        digitalWrite(step_pin[module], LOW);  
+        update_ms_pos(module);
       }
     }
-  }
-}
-
-void reset_home_pos(byte module){
-  // take care if destination is also home position
-  if(new_ms_pos[module] == ms_per_revolution){
-    if(cur_ms_pos[module] < new_ms_pos[module]){
-      // magnet has been spotted too early
-      ms_to_go[module] = 0;
-    }else if(cur_ms_pos[module] > new_ms_pos[module]){
-      // go extra round if too late
-      ms_to_go[module] = new_ms_pos[module];
-    }else{
-      // everything ok
-      ms_to_go[module] = 0;
-    }
-    cur_ms_pos[module] = 0;
-  }else{
-    // this is the normal case
-    cur_ms_pos[module] = 0;
-    set_ms_to_go(module);
   }
 }
 
@@ -167,32 +138,29 @@ void update_ms_pos(byte module){
   cur_ms_pos[module]++;
   ms_to_go[module]--;
 
+  // reset cur_ms_pos only once if magnet is at home place
   if(is_magnet_home(module)){
-    if(home_ms_pos[module] == init_ms_pos[module]){
-      reset_home_pos(module);
+    if(set_ms_pos[module]){
+      /*
+      if(cur_ms_pos[module] > ms_per_revolution){
+        // magnet behind counter
+      }
+      if(cur_ms_pos[module] < ms_per_revolution - 1){
+        // magnet before counter
+      }
+      */
+      cur_ms_pos[module] = 0;
+
+      // instant adjustment of microsteps to go
+      if(new_ms_pos[module] != ms_per_revolution){
+        set_ms_to_go(module);
+      }else{
+        ms_to_go[module] = init_ms_pos[module];
+      }
     }
-    home_ms_pos[module]++;
+    set_ms_pos[module] = false;
   }else{
-    home_ms_pos[module] = 0;
-  }
-
-  // is hall sensor actually responding?
-  if(cur_ms_pos[module] >= ms_per_revolution * 3){
-    Serial.println(" --> Hall sensor did not respond for 3 revolutions");
-    stats(module);
-    enabled[module] = false;
-  }
-}
-
-void update_flap_pos(byte module){
-  // flap position zwischen den ms positions einmitten
-  if((cur_ms_pos[module] + (ms_per_flap / 2)) % ms_per_flap == 0){
-    cur_flap_pos[module] = ((cur_ms_pos[module] + (ms_per_flap / 2)) / ms_per_flap);
-    if(new_flap_pos[module] > cur_flap_pos[module]){
-      flap_pos_to_go[module] = new_flap_pos[module] - cur_flap_pos[module];
-    }else{
-      flap_pos_to_go[module] = (flaps - cur_flap_pos[module]) + new_flap_pos[module];
-    }
+    set_ms_pos[module] = true;
   }
 }
 
@@ -212,29 +180,28 @@ new = 30
 (50 - 44) + 30 = 36
 */
 void set_ms_to_go(byte module){
-  new_flap_pos[module] = get_position(new_char[module]);
-  new_ms_pos[module] = new_flap_pos[module] * ms_per_flap;
+  word new_flap_pos = get_position(new_char[module]);
+  new_ms_pos[module] = new_flap_pos * ms_per_flap;
 
   // proceed only if a valid character has been provided
-  if(new_flap_pos[module] == 0){
+  if(new_flap_pos == 0){
     new_ms_pos[module] = 0;
     return;
   }
 
-  // pretend to be on correct position, if magnet has not been spotted
-  int counter = 0;
-  if(cur_ms_pos[module] > ms_per_revolution){
-    counter = cur_ms_pos[module] - ((cur_ms_pos[module] / ms_per_revolution) * ms_per_revolution);
-  }else{
-    counter = cur_ms_pos[module];    
-  }
+  // pretend to be on correct position, even if magnet has not been spotted
+  int position = cur_ms_pos[module] % ms_per_revolution;
 
   // do the magic!
-  if(new_ms_pos[module] > counter){
-    ms_to_go[module] = new_ms_pos[module] - counter;
+  if(new_ms_pos[module] > position){
+    ms_to_go[module] = new_ms_pos[module] + init_ms_pos[module] - position;
   }else{
-    ms_to_go[module] = (ms_per_revolution - counter) + new_ms_pos[module];
+    ms_to_go[module] = new_ms_pos[module] + init_ms_pos[module] + (ms_per_revolution - position);
   }
+}
+
+bool is_valid_module(byte module) {
+    return module < modules;
 }
 
 bool is_magnet_home(byte module){
@@ -282,27 +249,13 @@ void receive_i2c_event(int bytes){
 
 void request_i2c_event(){
   for(byte module = 0; module < modules; module++){
-    if(enabled[module]){
-      if(ms_to_go[module] > 0){
-        Wire.write(255);
-      }else{
-        Wire.write(cur_flap_pos[module]);
-      }
+    if(ms_to_go[module] > 0){
+      Wire.write(255);
     }else{
-      Wire.write(0);
+      // ToDo XXX
+      //Wire.write(cur_flap_pos[module]);
+      Wire.write(cur_ms_pos[module]);
     }
   }
-}
-
-void stats(byte module){
-  Serial.print(" module: ");
-  Serial.println(module);
-  Serial.print("   cur_ms_pos: ");
-  Serial.println(cur_ms_pos[module]);
-  Serial.print("   new_ms_pos: ");
-  Serial.println(new_ms_pos[module]);
-  Serial.print("   ms_to_go:   ");
-  Serial.println(ms_to_go[module]);
-  Serial.println("");
 }
 
