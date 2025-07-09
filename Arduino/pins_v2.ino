3ed434cce7b06dccf2cfd6842cac3bfd46c352fc
g000ze <stefan@frech.ch>
2025-07-09 07:25:20 +0200

    add missing pins_v2.ino

---
+++ Arduino/pins_v2.ino
@@ -0,0 +1,18 @@
+const byte hall_pin_0 = 18;
+const byte hall_pin_1 = 19;
+const byte hall_pin_2 = 20;
+const byte hall_pin_3 = 21;
+const byte hall_pin_4 = 9;
+const byte hall_pin_5 = 10;
+const byte hall_pin_6 = 11;
+const byte hall_pin_7 = 12;
+
+const byte step_pin_0 = 15;
+const byte step_pin_1 = 14;
+const byte step_pin_2 = 23;
+const byte step_pin_3 = 22;
+const byte step_pin_4 = 5;
+const byte step_pin_5 = 4;
+const byte step_pin_6 = 17;
+const byte step_pin_7 = 16;
+
