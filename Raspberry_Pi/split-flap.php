2b3b5c71e7bbbbbd88e7acf2a17fb0e421880eef
Stefan Frech <stefan@frech.ch>
2026-05-14 19:32:50 +0200

    correct return value for spinning modules

--- Raspberry_Pi/split-flap.php
+++ Raspberry_Pi/split-flap.php
@@ -380,7 +380,7 @@ function pos_to_walk($cur, $new, $same = false): int
         }
         else if($cur >= 255){
             // XXX ist das richtig???
-            return (255);
+            return (0);
         }
         else if($new > $cur)
         {
